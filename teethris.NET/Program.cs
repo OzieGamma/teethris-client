1fc483ad249d1e5a96ca0ba1614bd6c7837fb160
Oswald Maskens <ozie@live.be>
2016-03-12 08:32:19 +0100

    Added hooks

--- teethris.NET/Program.cs
+++ teethris.NET/Program.cs
@@ -1,15 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// <copyright company="Oswald MASKENS" file="Program.cs">
+// Copyright 2014-2016 Oswald MASKENS
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+// 
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+// </copyright>
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace teethris.NET
 {
-    public class Program
+    internal class InterceptKeys
     {
-        public static void Main(string[] args)
+        private const int WhKeyboardLl = 13;
+        private const int WmKeydown = 0x0100;
+        private static readonly LowLevelKeyboardProc Proc = HookCallback;
+        private static IntPtr hookId = IntPtr.Zero;
+
+        public static void Main()
         {
-            Console.WriteLine("Hello World !");
+            hookId = SetHook(Proc);
+            Application.Run();
+            UnhookWindowsHookEx(hookId);
         }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (var curProcess = Process.GetCurrentProcess())
+            using (var curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WhKeyboardLl, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(
+            int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(
+            int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if ((nCode >= 0) && (wParam == (IntPtr) WmKeydown))
+            {
+                var vkCode = Marshal.ReadInt32(lParam);
+                Console.WriteLine(vkCode);
+            }
+            return CallNextHookEx(hookId, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
     }
 }
