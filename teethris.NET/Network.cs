9bf2b21cf07390fc3589114344d689c714e527ac
Boris Gordts <borisgordts@hotmail.com>
2016-03-12 09:43:09 +0100

    Network!

---
+++ teethris.NET/Network.cs
@@ -0,0 +1,21 @@
+using Quobject.SocketIoClientDotNet.Client;
+
+namespace teethris.NET
+{
+    public class Network
+    {
+        public Network(){
+            var socket = IO.Socket("http://borisjeltsin.azurewebsites.net/");
+            socket.On(Socket.EVENT_CONNECT, () =>
+            {
+                socket.Emit("chat message", "hi");
+            });
+
+            socket.On("chat message", (data) =>
+                {
+                    Console.WriteLine(data);
+                    socket.Disconnect();
+                });
+        }
+    }
+}
